6bfa984d82adf1be4f6daab0472624acc7fa8ce2
rvuddanti <rvuddanti@RVUDDANTI-LP>
2022-11-29 13:37:06 +0530

    Added finally

--- TryCatchFinallyExample/Program.cs
+++ TryCatchFinallyExample/Program.cs
@@ -17,7 +17,7 @@ namespace TryCatchFinallyExample
 
             try //try and catch is used to insted of crashing the program we will tell the user what to do. Here if the user enter the string then we print enter correct format.
             {
-                int userINputAsInt = int.Parse(userInput);// place the cu
+                int userINputAsInt = int.Parse(userInput);// place the cursor on parse to know the possible exceptions
             }
             catch (FormatException) // here why we use format exception insted of normal exception is because the following line in try throw a FORMAT EXCEPTION.
             {
@@ -28,6 +28,10 @@ namespace TryCatchFinallyExample
             catch(OverflowException) // since we entered Format exception in above line if overfolw exception came it wont handle so we need to write separate exception for this.
             {
                 Console.WriteLine("the number you entered is too big for a int32");
+            }
+            finally
+            {
+                Console.WriteLine("This is called anyways");
             }
             Console.ReadKey();
         }
