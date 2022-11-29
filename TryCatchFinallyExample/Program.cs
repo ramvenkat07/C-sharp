1012b5b3bd042cf58f40f20609cba02b4b1b9550
ramvenkat07 <111556104+ramvenkat07@users.noreply.github.com>
2022-11-29 13:30:56 +0530

    Add files via upload

---
+++ TryCatchFinallyExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinallyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number");
+            string userInput = Console.ReadLine();
+
+
+            try //try and catch is used to insted of crashing the program we will tell the user what to do. Here if the user enter the string then we print enter correct format.
+            {
+                int userINputAsInt = int.Parse(userInput);// place the cu
+            }
+            catch (FormatException) // here why we use format exception insted of normal exception is because the following line in try throw a FORMAT EXCEPTION.
+            {
+                //throw; // if we place throw it will trow the error
+                Console.WriteLine("Format exception, please enter the correct type next time");
+
+            }
+            catch(OverflowException) // since we entered Format exception in above line if overfolw exception came it wont handle so we need to write separate exception for this.
+            {
+                Console.WriteLine("the number you entered is too big for a int32");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ json/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myJsonString = File.ReadAllText(@"C:\Users\rvuddanti\source\repos\json\sample2.json");
+            var myJObject = JObject.Parse(myJsonString);
+            Console.WriteLine(myJObject.SelectToken("firstName").Value<string>());
+            Console.WriteLine(myJObject.SelectToken(""));
+        }
+    }
+}
