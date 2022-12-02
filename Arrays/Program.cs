f9f16c756c0e7e0d7575ba0b086bc2be154bf282
rvuddanti <rvuddanti@RVUDDANTI-LP>
2022-12-02 12:11:42 +0530

    Arryas script added

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,18 +8,23 @@ namespace Arrays
 {
     class Program
     {
-       public static int[] Marks = new int[5];
+       public static int[] Marks = new int[5]; // global variable
 
-        public int[] Markss = { 1, 23, 24, 22, 23, 33 }; // this is another type.
 
-        public int[] Markssss = new int[3] { 12, 2, 3 }; // another type.
+      
         static void Main(string[] args)
         {
+         int[] Markssss = new int[3] { 12, 2, 3 }; // another type.
+         int[] Markss = { 1, 23, 24, 22, 23, 33 }; // this is another type.  // Can be only used in main
+
             Marks[0] = 5;
             Marks[1] = 10;
             Marks[2] = 12;
             Marks[3] = 15;
             Marks[4] = 16;// This is one type of initialization.
+
+            Console.WriteLine("Lenght of an array is "+ Marks.Length);
+            Console.ReadKey();
         }
     }
 }
