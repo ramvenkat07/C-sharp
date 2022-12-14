eb5b7e7ff632457464cc27b6165a6115a035426a
rvuddanti <rvuddanti@RVUDDANTI-LP>
2022-12-14 15:34:01 +0530

    Added jagged arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,28 +8,29 @@ namespace Arrays
 {
     class Program
     {
-       public static int[] Marks = new int[5]; // global variable
+        public static int[] Marks = new int[5]; // global variable
 
-      
+
         static void Main(string[] args)
         {
-         int[] Markssss = new int[3] { 12, 2, 3 }; // another type of initialization.
-         int[] Markss = { 1, 23, 24, 22, 23, 33 }; // this is another type.  // Can be only used in main since not using static brfore.
+            //int[] Markssss = new int[3] { 12, 2, 3 }; // another type of initialization.
+            //int[] Markss = { 1, 23, 24, 22, 23, 33 }; // this is another type.  // Can be only used in main since not using static brfore.
 
-            Marks[0] = 5;
-            Marks[1] = 10;
-            Marks[2] = 12;
-            Marks[3] = 15;
-            Marks[4] = 16;// This is one type of initialization.
+            //Marks[0] = 5;
+            //Marks[1] = 10;
+            //Marks[2] = 12;
+            //Marks[3] = 15;
+            //Marks[4] = 16;// This is one type of initialization.
 
-            Console.WriteLine("Lenght of an array is "+ Marks.Length);
-            Console.ReadKey();
-            TwoDimensionalArray();
-            ThreeDimensionalArrays();
-            
+            //Console.WriteLine("Lenght of an array is " + Marks.Length);
+            //Console.ReadKey();
+            //TwoDimensionalArray();
+            //ThreeDimensionalArrays();
+            JaggedArray();
+
         }
 
-       static void TwoDimensionalArray()
+        static void TwoDimensionalArray()
         {
             int[,] array2D = new int[,]
             {
@@ -47,25 +48,25 @@ namespace Arrays
             };
 
 
-            for(int i= 0;i< array2D.GetLength(0);i++) // nested for loop
+            for (int i = 0; i < array2D.GetLength(0); i++) // nested for loop
             {
-                for(int j = 0; j<array2D.GetLength(1);j++)
+                for (int j = 0; j < array2D.GetLength(1); j++)
                 {
                     array2D[1, 1] = 234;
-                    Console.Write(array2D[i,j]);
+                    Console.Write(array2D[i, j]);
                     Console.Write("  ");
                 }
                 Console.WriteLine("\n");
             }
 
-            foreach(int item in array2D) // using foreach  (this is super useful to just read the values not for changing or adding liked nested loops)
+            foreach (int item in array2D) // using foreach  (this is super useful to just read the values not for changing or adding liked nested loops)
             {
-                Console.WriteLine("The items of array are "+item);
+                Console.WriteLine("The items of array are " + item);
             }
 
 
-            Console.WriteLine("The center value is "+array2D[1,0]);
-            Console.WriteLine("The length of the array is "+ array2D.Length);
+            Console.WriteLine("The center value is " + array2D[1, 0]);
+            Console.WriteLine("The length of the array is " + array2D.Length);
             Console.ReadKey();
         }
 
@@ -83,8 +84,42 @@ namespace Arrays
                 },
             };
 
-            Console.WriteLine( "The lenght of a 3D array is "+ array3D.Length);
+            Console.WriteLine("The lenght of a 3D array is " + array3D.Length);
             Console.WriteLine("The value of back first row 1 element is " + array3D[1, 1, 0]);
+            Console.ReadKey();
+        }
+
+        static void JaggedArray()  // These jagged arrays are called arrays inside arrays
+        {
+
+            // index                        0     1    2
+            // normal array of type int  : [12],[21],[22]
+            // index                                      0                     1                       2
+            //jagged array of type int  : [array1([12],[21],[22]), array2([162],[27],[22]), array3([2],[21],[2])]
+
+            //One way of declaring the jagged arrays
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[4];
+            jaggedArray[1] = new int[4];
+            jaggedArray[2] = new int[2];
+
+            jaggedArray[0] = new int[] { 1, 2, 3, 4 };
+            jaggedArray[1] = new int[] { 1, 2, 3, 4, };
+            jaggedArray[2] = new int[] { 1, 2};
+
+            Console.WriteLine("The center value{0}", jaggedArray[0][1]);
+            Console.ReadKey();
+
+            //alternate way of declaring jagged arrays
+
+            int[][] jaggedarray2 = new int[][]
+            {
+               new int[] {1,2,3,4},
+               new int[] {3,4,5,3,3,2 }
+            };
+
+            Console.WriteLine("the value of 1st middle is {0}", jaggedarray2[0][2]);
             Console.ReadKey();
         }
     }
