1a2ad1eb861c776a13cc0c64be4bfa2b26c707c6
rvuddanti <rvuddanti@RVUDDANTI-LP>
2023-01-04 12:33:52 +0530

    Added new programs

---
+++ Try and catch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy
+{
+    class Program
+    {
+        static int age;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your age???");
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Please enter a number...");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Age is " + age);
+            Console.ReadLine();
+        }
+    }
+}
