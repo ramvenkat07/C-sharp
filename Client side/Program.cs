b60b43221bf028eabc504473c276dece22d4b435
rvuddanti <rvuddanti@RVUDDANTI-LP>
2023-01-11 13:41:25 +0530

    TCP scripts

---
+++ Client side/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client_side
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Socket client = null;
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = null;
+
+            try
+            {
+                Console.WriteLine("Welcome to Socket client starter");
+                Console.WriteLine("Please enter a valid ip address and press enter");
+
+                string strIPAddress = Console.ReadLine();
+
+                Console.WriteLine("Please supply a valid port number 0-65535 and press enter");
+                String srtPortInput = Console.ReadLine();
+                int nPortInput = 0;
+
+               if(!IPAddress.TryParse(strIPAddress, out ipaddr))
+               {
+                    Console.WriteLine("Invalid server Ip supplied");
+               }
+                if(!int.TryParse(srtPortInput.Trim(), out nPortInput))
+               {
+                    Console.WriteLine("Invalid port number supplied");
+               }
+                client.Connect(ipaddr,nPortInput);
+
+                Console.WriteLine("Connected to the server, type text and press enter to sent it to server,type <Exit> to close.");
+
+                string inputCommand = string.Empty;
+
+                while(true)
+                {
+                    inputCommand = Console.ReadLine();
+                    if(inputCommand.Equals("<EXIT>"))
+                    {
+                        break;
+                    }
+
+                   byte[] buffsend = Encoding.ASCII.GetBytes(inputCommand);
+
+                    client.Send(buffsend);
+
+                    byte[] buffReceived = new byte[128];
+                    int nRecv = client.Receive(buffReceived);
+
+             
+                }
+                   
+            }
+
+            catch (Exception excp)
+            {
+                Console.WriteLine(excp.ToString());
+            }
+
+            finally
+            {
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                client.Dispose();
+            }
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ server side/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenersocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+           
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr,13);
+            
+
+            listenersocket.Bind(ipep);
+
+            listenersocket.Listen(5);
+
+            Console.WriteLine("About to accept incoming conection");
+
+           Socket client = listenersocket.Accept();
+
+            Console.WriteLine("Client connected. " + client.ToString() + " -Ip End point : " + client.RemoteEndPoint.ToString());
+
+            byte[] buff = new byte[128];
+            int numberofReceivedBytes = 0;
+            while(true)
+            {
+                numberofReceivedBytes = client.Receive(buff);
+
+                Console.WriteLine("Data sent by client is : " + buff);
+
+                string receivedText = Encoding.ASCII.GetString(buff, 0, numberofReceivedBytes);
+
+                Console.WriteLine("Data send by client is: "+receivedText);
+                client.Send(buff);
+
+                if(receivedText == "X")
+                {
+                    break;
+                }
+
+                Array.Clear(buff, 0, buff.Length);
+                numberofReceivedBytes = 0;
+            }
+            
+
+
+
+        }
+    }
+}
